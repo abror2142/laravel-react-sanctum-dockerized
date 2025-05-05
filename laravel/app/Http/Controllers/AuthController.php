c272f4e5033c85aa73ee71468463ef2985776a8a
Abrorbek Ismoilov <107830282+abror2142@users.noreply.github.com>
2025-05-05 16:12:21 +0500

    Authenticating user: AuthController implementation

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -19,5 +20,30 @@ class AuthController extends Controller
         }
 
         return response()->json(['message' => 'Invalid credentials are provided!']);
+    }
+
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => ['required', 'max:255'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed']
+        ]);
+
+        if(User::find(['email' => $request->email])->exists()){
+            return response()->json(['message' => 'This email is already taken.'], 400);
+        }
+
+        $user = User::create($fields);
+        $user->createToken($request->device_name)->plainTextToken;
+
+        return response()->json(['message' => 'Successfully registered!', 'user' => $user]);
+    }
+
+    public function logout()
+    {
+        $user = auth()->user();
+        $user->tokens()->delete();
+        return response()->json(['message' => 'Successfully logged out!']);
     }
 }
